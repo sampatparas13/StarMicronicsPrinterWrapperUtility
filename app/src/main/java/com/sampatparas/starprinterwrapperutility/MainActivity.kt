a62c49eaf8372a9b820d13ffd5bf37bb657e756e
Narendra Motwani <narendra.samcomtechnobrains@gmail.com>
2022-04-27 12:41:40 +0530

    Sample added

--- app/src/main/java/com/sampatparas/starprinterwrapperutility/MainActivity.kt
+++ app/src/main/java/com/sampatparas/starprinterwrapperutility/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.sampatparas.starprinterwrapperutility
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.sampatparas.starprinterutility.searchPrinter.SearchPrinterUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val s = SearchPrinterUtils(
+            this
+        ) { result ->
+            for (info in result) {
+                Toast.makeText(this@MainActivity, "" + info.modelName, Toast.LENGTH_SHORT).show()
+                Log.e(
+                    MainActivity::class.java.simpleName,
+                    "callback: " + info.modelName
+                )
+            }
+        }
+        s.startSearch("LANsdasd")
     }
 }
